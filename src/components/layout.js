5f2951357e654816c0f9e9a2884db27c3e73ad53
huyvouit <huyvouit@gmail.com>
2022-02-15 22:14:39 +0700

    landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./header/header";
-import Footer from "./footer";
+import Footer from "./footer/footer";
 const Layout = () => {
   return (
     <div>
